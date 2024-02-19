10dda910dabca96ab1f4d69facfe806ae372da0f
anniemcmahon <anniemcmahon20@gmail.com>
2024-02-19 15:39:37 -0500

    added CSS

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import "./Button.css";
 
 export interface ButtonProps{
     label: string;
 }
 
 const Button = ( {label}: ButtonProps) => {
-    return <button>{label}</button>
+    return <button className="btn">{label}</button>
 }
 
 export default Button;
